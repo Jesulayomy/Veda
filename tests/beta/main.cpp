0bc26964957ec58af02f2f3dc10995aef0435b9a
jesulayomy <jesulayomi081@gmail.com>
2023-08-02 20:18:16 +0100

    Test beta version with buttons

--- tests/beta/main.cpp
+++ tests/beta/main.cpp
@@ -5,10 +5,11 @@
 #define b2 3
 #define b3 4
 #define b4 5
+#define b5 6
 
 struct Student {
  String name;
- int matric;
+ long int matric;
 };
 
 LiquidCrystal_I2C lcd(0x20, 16, 2); //  Declare 1602 LCD screen confix at 0x20 address
@@ -32,16 +33,20 @@ void setup()
 void loop() {
   Student student1 = {"Aina", 20171370};
   Student student2 = {"Adexy", 20171372};
+  Student student3 = {"Kenny", 20171374};
 
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Stand By");
   Serial.println("Stand By");
-  while ((digitalRead(b1) == LOW) && (digitalRead(b4) == LOW) && (digitalRead(b3) == LOW) && (digitalRead(b2) == LOW)) {
+
+  // Wait till a button is pressed
+  while ((digitalRead(b1) == LOW) && (digitalRead(b2) == LOW) && (digitalRead(b3) == LOW) && (digitalRead(b4) == LOW) && (digitalRead(b5) == LOW)) {
     delay(100);
   }
 
-  while ((digitalRead(b1) == HIGH) || (digitalRead(b4) == HIGH) || (digitalRead(b2) == HIGH) || (digitalRead(b3) == HIGH)) {
+  // Perform  an action depending on which button is pressed
+  while ((digitalRead(b1) == HIGH) || (digitalRead(b2) == HIGH) || (digitalRead(b3) == HIGH) || (digitalRead(b4) == HIGH) || (digitalRead(b5) == HIGH)) {
   	if (digitalRead(b1) == HIGH) {
    	  Serial.println("Deleting a student");
       lcd.clear();
@@ -49,61 +54,71 @@ void loop() {
       lcd.print(student2.name);
       lcd.setCursor(9, 1);
       lcd.print("Deleted");
-      delay(100);
+      delay(1000);
       continue;
   	}
   	else if (digitalRead(b2) == HIGH) {
-      Serial.println("Registering newstudent");
+      Serial.println("Registering new student");
       lcd.clear();
       lcd.setCursor(0, 0);
       lcd.print("New Student");
       lcd.setCursor(0, 1);
       lcd.print(student1.name);
-      delay(100);
+      delay(1000);
   	  continue;
     }
   	else if (digitalRead(b3) == HIGH) {
       Serial.println("Reset was pressed");
       lcd.clear();
       lcd.setCursor(0, 0);
-      lcd.print("Reset Data");
-      delay(100);
+      lcd.print("Resetting Data");
+      delay(1000);
       continue;
   	}
   	else if (digitalRead(b4) == HIGH) {
       Serial.println("New finger Placed");
       Serial.println("Press b1 to Cancel or b2 to Register");
-      while ((digitalRead(b1) == LOW) && (digitalRead(b3) == LOW) && (digitalRead(b2) == LOW)) {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("b1: Cancel");
+      lcd.setCursor(0, 1);
+      lcd.print("b2: Register");
+      while ((digitalRead(b1) == LOW) && (digitalRead(b2) == LOW)) {
         delay(100);
   	  }
 
-      while ((digitalRead(b1) == HIGH) || (digitalRead(b2) == HIGH) || (digitalRead(b3) == HIGH)) {
+      while ((digitalRead(b1) == HIGH) || (digitalRead(b2) == HIGH)) {
   	    if (digitalRead(b1) == HIGH) {
    	      Serial.println("Cancel new student Registration");
           lcd.clear();
           lcd.setCursor(0, 0);
           lcd.print("Cancelled");
-          delay(100);
+          delay(500);
   	    }
   	    else if (digitalRead(b2) == HIGH) {
           Serial.println("New student added");
           lcd.clear();
           lcd.setCursor(0, 0);
           lcd.print("Registration");
-          lcd.setCursor(0, 1);r
+          lcd.setCursor(0, 1);
           lcd.print("Successful");
-          delay(100);
-  	    }
-  	    else if (digitalRead(b3) == HIGH) {
-          Serial.println("Do not press Reset!");
-          lcd.clear();
-          lcd.setCursor(0, 0);
-          lcd.print("Operation");
-          lcd.setCursor(6, 1);
-          lcd.print("Cancelled");
-          delay(100);
+          delay(500);
   	    }
       }
+  	}
+  	else if (digitalRead(b5) == HIGH) {
+      Serial.println("Admit existing student");
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Welcome");
+      lcd.setCursor(8, 0);
+      lcd.print(student3.name);
+      lcd.setCursor(0, 1);
+      lcd.print(student3.matric);
+      lcd.setCursor(9, 1);
+      lcd.print("30-JUL");
+      delay(1000);
+      continue;
   	}
   }
 }
